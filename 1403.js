e220fd80de091ccaa2ca3771fc948c35301e7c40
jasmine2609 <54301309+jasmine2609@users.noreply.github.com>
2019-08-21 16:43:55 +0700

    Create 1403.js

---
+++ 1403.js
@@ -0,0 +1,32 @@
+/**
+ * Sử dụng vòng lặp for...in để in ra tất cả các key trong object sau, in ra cả các key của các nested object (object con lồng bên trong object lớn)
+ * Gợi ý: đây là bài tập khó, bạn nên tìm hiểu về recursive trước
+ */
+
+var apartment = {
+  bedroom: {
+    area: 20,
+    bed: {
+      type: 'twin-bed',
+      price: 100
+    }
+  }
+};
+function printKey(object) {
+  for (var key in object) {
+    console.log(key);
+    if (object[key] instanceof Object) {
+      printKey(object[key]);
+    }
+  }
+}
+printKey(apartment);
+/**
+ * Kết quả mong muốn:
+ * bedroom
+ * area
+ * bed
+ * type
+ * price
+ * Chú ý: không cần hiển thị ra đúng thứ tự như trên
+ */
