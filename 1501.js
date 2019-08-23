636e4e46f1dea7d96427fa31fd336464d8e4e790
jasmine2609 <54301309+jasmine2609@users.noreply.github.com>
2019-08-23 16:47:31 +0700

    Create 1501.js

---
+++ 1501.js
@@ -0,0 +1,9 @@
+function removeEnd(arr, n) {
+arr.length -= n;
+return arr; 
+}
+console.log(removeEnd([2, 3, 1, 8, 9, 7], 3));
+
+/*
+	Viết 1 chương trình xóa đi n phần tử cuối cùng của 1 array
+*/
