014b31cbe499228b168243d6fda7f75aaf6d6977
jasmine2609 <54301309+jasmine2609@users.noreply.github.com>
2019-08-23 14:58:55 +0700

    Create 1405.js

---
+++ 1405.js
@@ -0,0 +1,19 @@
+function has(object, key) {
+for (var a in object){
+	if (key === a){
+		return true;
+			}
+		else{
+			return false;
+					}
+					}
+          
+
+  // viết hàm has để kiểm tra xem object có tồn tại 1 key xác định hay không
+  // Tham số:
+  //	- object: object cần kiểm tra
+  //	- key: key cần kiểm tra xem có tồn tại trong object không
+  // Return:
+  //	- true nếu có tồn tại
+  //	- false nếu không tồn tại
+}
