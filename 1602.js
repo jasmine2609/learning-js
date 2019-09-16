c33cffaa5e538b8ffb473f8e1bcd4d9d06333469
jasmine2609 <54301309+jasmine2609@users.noreply.github.com>
2019-09-16 14:21:17 +0700

    Update 1602.js

--- 1602.js
+++ 1602.js
@@ -1,6 +1,7 @@
 /**
  * 1. Viết hàm double nhận vào 1 số bất kì, hiển thị ra số đó nhân đôi
- * 2. Viết hàm sumAndDo nhận vào 1 array và 1 callback function. Hàm sumAndDo làm nhiệm vụ tính tổng array đó, sau đó gọi callback function với tham số là kết quả tổng vừa tính
+ * 2. Viết hàm sumAndDo nhận vào 1 array và 1 callback function. Hàm sumAndDo làm 
+ nhiệm vụ tính tổng array đó, sau đó gọi callback function với tham số là kết quả tổng vừa tính
  */
 
 function double(num) {
